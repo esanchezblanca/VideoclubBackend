ac48daa48ec5e4df4e52780e1de78de0349993f2
Elena <esanchez6@gmail.com>
2020-10-24 15:04:29 +0200

    creado endpoint películas

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
-const movies = {
+const movies = [
     
     {id: 1, title: 'Shrek'},
     {id: 2, title: 'El príncipe de Zamunda'},
@@ -25,26 +25,19 @@ const movies = {
     {id: 17, title: 'Forrest Gump'},
     {id: 18, title: 'La Vida es bella'},
     {id: 19, title: 'Intocable'},
-    {id: 20, title: 'Cadena Perpetua'},
-    
-    
-    
-    
-    let m5 = new Movies("ID005", "El caballero oscuro", 2008, "acción", "Christian Bale", "https://pics.filmaffinity.com/El_caballero_oscuro-102763119-large.jpg");
-    let m6 = new Movies("Id006", "Mad Max: Fury Road", 2015, "acción", "Charlize Theron", "https://www.websweepuk.com/ebay_images/Mad-Max-Fury-Road-us-one-sheet-buy-original-movie-posters-at-starstills.jpg");
-    let m7 = new Movies("ID007", "Solo en casa", 1990, "comedia", "Macaulay Culkin", "https://cartelera.elpais.com/assets/uploads/2018/12/21030155/F_12929.jpg");
-    let m8 = new Movies("ID008", "Django desencadenado", 2012, "drama", "Jamie Foxx", "https://images-na.ssl-images-amazon.com/images/I/51dNfU53lNL._AC_.jpg");
-    let m9 = new Movies("ID009", "Casablanca", 1942, "drama", "Humpfrey Bogart", "https://e.snmc.io/i/300/w/77330ee70810f141e075acff98852f53/6595852");
-    let m10 = new Movies("ID010", "IT", 1990, "terror", "Tim Curry", "https://images-na.ssl-images-amazon.com/images/I/513lqk2Bz9L.jpg");
-    let m11 = new Movies("ID011", "Donnie Darko", 2001, "suspense", "Jake Gyllenhaal", "https://pics.filmaffinity.com/Donnie_Darko-454757435-large.jpg");
-    
-    
-    
-    
-    
-
-
-
+    {id: 20, title: 'Cadena Perpetua'}
+]  
 app.get('/', (req, res) => res.send('EXPRESS'));
+
+app.get('/movies', (req, res) =>{
+    res.json(movies);
+});
+
+app.get('/movies/:id' , (req, res) => {
+    const { id } = req.params;
+    let movie = movies.find(movie => movie.id == id);
+    res.json(movie);
+});
+
 
 app.listen(PORT, () => console.log('Server is working'))
