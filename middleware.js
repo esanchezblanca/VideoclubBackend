1400dd44fe2170b7ab507cbd8212ae455f362933
Elena <esanchez6@gmail.com>
2020-11-12 00:00:54 +0100

    subida a heroku

--- middleware.js
+++ middleware.js
@@ -1,18 +1,18 @@
-const config = require('./components/user/config');
+const config = require('./components/user/config'); //La ruta la tienes que cambiar y si tienes que salirte de la carpeta usas ../
 const jwt = require('jsonwebtoken');
 module.exports = {
     tokenMiddle: function (req, res, next) {
         const token = req.headers['access-token'];
-        if (token) {
+        if (token) { //si me das el token
             jwt.verify(token, config.key, (err, decoded) => {
-                if (err) {
+                if (err) { // si me metes token que no vale
                     return res.json({ mensaje: 'Token inválida' });
                 } else {
                     req.decoded = decoded;
                     next();
                 }
             });
-        } else {
+        } else { //si no me metes token
             res.send({
                 mensaje: 'Token no proveída.'
             });
